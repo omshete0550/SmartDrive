b78ed60c384ff0f5c61bd41c58d78f65a0bc02a6
Om <omshete0550@gmail.com>
2024-05-01 15:09:32 +0530

    updates

---
+++ Drinking-detection/main.py
@@ -0,0 +1,32 @@
+from ultralytics import YOLO
+import cvzone
+import cv2
+import math
+
+# Running real time from webcam
+cap = cv2.VideoCapture('../mohibtemp.mp4')
+model = YOLO('./best.pt')
+
+# Reading the classes
+classnames=['Drinking']
+
+while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame,(640,480))
+    result = model(frame, stream=True)
+
+    for info in result:
+        boxes = info.boxes
+        for box in boxes:
+            connfidence = box.conf[0]
+            connfidence = math.ceil(connfidence * 100)
+            Class = int(box.cls[0])
+            if connfidence > 50:
+                x1,y1,x2,y2 = box.xyxy[0]
+                x1, y1, x2, y2 = int(x1),int(y1),int(x2),int(y2)
+                cv2.rectangle(frame,(x1,y1),(x2,y2),(0,0,255),5)
+                cvzone.putTextRect(frame, f'{classnames[Class]} {connfidence}%', [x1+8, y1+100], scale=1.5, thickness=2)
+                
+
+    cv2.imshow('frame', frame)
+    cv2.waitKey(1)
