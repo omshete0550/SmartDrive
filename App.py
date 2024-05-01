cda1146b1471a64a5bc77bcbbcb8e469f21648e9
Hamza Sayyed <hamzawp404@gmail.com>
2024-05-01 23:27:01 +0530

    occupancy integrated

--- App.py
+++ App.py
@@ -85,92 +85,15 @@ model = torch.hub.load(
 )
 smoking_model = YOLO("./Smoking-detection/cigar.pt")
 drinking_model = YOLO("./Smoking-detection/best.pt")
-classnames_cigar = ['cigarette']
-classnames_drink = ['drinking', 'smoking']
+classnames_cigar = ["cigarette"]
+classnames_drink = ["drinking", "smoking"]
 cap = cv2.VideoCapture(INPUT_VIDEO)
 frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
-
-
-# def draw_dashboard(img, smoking_detected, drinking_detected, seatbelt_detected, drowsy):
-#     dashboard_color = (0, 0, 0)
-#     dashboard_height = 100
-#     cv2.rectangle(img, (0, 0), (output_width, dashboard_height), dashboard_color, -1)
-#     cv2.putText(
-#         img, "Dashboard", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2
-#     )
-#     indicator_size = 20
-#     indicator_start_x = 150
-#     cv2.putText(
-#         img,
-#         "Smoking:",
-#         (indicator_start_x, 50),
-#         cv2.FONT_HERSHEY_SIMPLEX,
-#         0.5,
-#         (255, 255, 255),
-#         2,
-#     )
-#     cv2.putText(
-#         img,
-#         "Drinking:",
-#         (indicator_start_x, 75),
-#         cv2.FONT_HERSHEY_SIMPLEX,
-#         0.5,
-#         (255, 255, 255),
-#         2,
-#     )
-#     cv2.putText(
-#         img,
-#         "Seatbelt:",
-#         (indicator_start_x, 100),
-#         cv2.FONT_HERSHEY_SIMPLEX,
-#         0.5,
-#         (255, 255, 255),
-#         2,
-#     )
-#     cv2.putText(
-#         img,
-#         "Drowsy:",
-#         (indicator_start_x + 270, 75),
-#         cv2.FONT_HERSHEY_SIMPLEX,
-#         0.5,
-#         (255, 255, 255),
-#         2,
-#     )
-#     cv2.rectangle(
-#         img,
-#         (indicator_start_x + 70, 35),
-#         (indicator_start_x + 70 + indicator_size, 35 + indicator_size),
-#         (0, 255, 0) if smoking_detected else (0, 0, 255),
-#         -1,
-#     )
-#     cv2.rectangle(
-#         img,
-#         (indicator_start_x + 70, 60),
-#         (indicator_start_x + 70 + indicator_size, 60 + indicator_size),
-#         (0, 255, 0) if drinking_detected else (0, 0, 255),
-#         -1,
-#     )
-#     cv2.rectangle(
-#         img,
-#         (indicator_start_x + 70, 85),
-#         (indicator_start_x + 70 + indicator_size, 85 + indicator_size),
-#         (0, 255, 0) if seatbelt_detected else (0, 0, 255),
-#         -1,
-#     )
-#     cv2.rectangle(
-#         img,
-#         (indicator_start_x + 340, 60),
-#         (indicator_start_x + 340 + indicator_size, 60 + indicator_size),
-#         (0, 255, 0) if not drowsy else (0, 0, 255),
-#         -1,
-#     )
-
 frame_rate = cap.get(cv2.CAP_PROP_FPS)
 seconds_between_frames = 1 / frame_rate
 frame_count = 0
 try:
-    # start_time = time.time()
     while True:
         ret, img = cap.read()
         if ret:
@@ -182,6 +105,29 @@ try:
                 img = limit_roi(
                     img, x_min=0, y_min=0, x_max=output_width, y_max=output_height
                 )
+
+                # Detect faces and count them
+                gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+                faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+                face_count = len(faces)
+
+                # Draw rectangles around the faces
+                for x, y, w, h in faces:
+                    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+                # Display face count on the video
+                cv2.putText(
+                    img,
+                    f"Faces: {face_count}",
+                    (10, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (255, 255, 255),
+                    2,
+                )
+
+                # Log the face count
+                log_activity(f"Faces detected: {face_count} at frame {frame_count}")
 
                 # Detect driver seatbelt
                 results = model(img)
@@ -217,7 +163,6 @@ try:
                 # Detect smoking and drinking
                 result_cigar = smoking_model(img, stream=True)
                 result_drink = drinking_model(img, stream=True)
-                # result = smoking_drinking_model(img, stream=True)
                 smoking_detected = False
                 drinking_detected = False
                 for info in result_cigar:
@@ -231,8 +176,16 @@ try:
                             x1, y1, x2, y2 = box.xyxy[0]
                             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
                             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
-                            cvzone.putTextRect(img, f'{classnames_cigar[Class]} {confidence}%', [x1 + 8, y1 + 100], scale=1, thickness=1)
-                            log_activity(f"Smoking detected at frame {frame_count} with confidence {confidence}%")
+                            cvzone.putTextRect(
+                                img,
+                                f"{classnames_cigar[Class]} {confidence}%",
+                                [x1 + 8, y1 + 100],
+                                scale=1,
+                                thickness=1,
+                            )
+                            log_activity(
+                                f"Smoking detected at frame {frame_count} with confidence {confidence}%"
+                            )
 
                 for info in result_drink:
                     boxes = info.boxes
@@ -245,14 +198,18 @@ try:
                             x1, y1, x2, y2 = box.xyxy[0]
                             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
                             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                            cvzone.putTextRect(img, f'{classnames_drink[Class]} {confidence}%', [x1 + 8, y1 + 100], scale=1, thickness=1)
-                            log_activity(f"Drinking detected at frame {frame_count} with confidence {confidence}%")
-
-
+                            cvzone.putTextRect(
+                                img,
+                                f"{classnames_drink[Class]} {confidence}%",
+                                [x1 + 8, y1 + 100],
+                                scale=1,
+                                thickness=1,
+                            )
+                            log_activity(
+                                f"Drinking detected at frame {frame_count} with confidence {confidence}%"
+                            )
 
                 # Drowsiness detection
-                gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-                faces = face_cascade.detectMultiScale(gray, 1.3, 5)
                 drowsy = False
                 for x, y, w, h in faces:
                     roi_gray = gray[y : y + h, x : x + w]
@@ -284,57 +241,58 @@ try:
                             1,
                         )
 
-                # draw_dashboard(
-                #     img, smoking_detected, drinking_detected, seatbelt_detected, drowsy
-                # )
-
                 img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
                 cv2.namedWindow("Video feed", cv2.WINDOW_NORMAL)
                 cv2.resizeWindow("Video feed", output_width, output_height)
                 cv2.imshow("Video feed", img)
-
-                # log_activity(f"Seatbelt worn at time {elapsed_time}s") if seatbelt_detected else None
-                # log_activity(f"Seatbelt not worn detected at time {elapsed_time}s") if not seatbelt_detected else None
-                # log_activity(f"Smoking detected at time {elapsed_time}s with confidence {confidence}%") if smoking_detected else None
-                # log_activity(f"Drinking detected at time {elapsed_time}s with confidence {confidence}%") if drinking_detected else None
-                # log_activity(f"Drowsiness detected at time {elapsed_time}s") if drowsy else None
-
         else:
             break
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
+
 except KeyboardInterrupt:
     pass
 finally:
+    # Post-processing and report generation
     seatbelt_count = 0
     seatbelt_notcount = 0
     smoking_detected = False
     drinking_detected = False
     drowsiness_detected = False
+    face_count = 0
 
-    with open('activity_log.txt', 'r') as file:
+    with open("activity_log.txt", "r") as file:
         lines = file.readlines()
 
     # Process the log file
     for line in lines:
-        if 'Seatbelt worn detected' in line:
+        if "Seatbelt worn detected" in line:
             seatbelt_count += 1
-        elif 'Seatbelt not worn' in line:
+        elif "Seatbelt not worn" in line:
             seatbelt_notcount += 1
-        elif 'Smoking detected' in line:
+        elif "Smoking detected" in line:
             smoking_detected = True
-        elif 'Drinking detected' in line:
+        elif "Drinking detected" in line:
             drinking_detected = True
-        elif 'Drowsiness detected' in line:
+        elif "Drowsiness detected" in line:
             drowsiness_detected = True
+        elif "Faces detected" in line:
+            new_face_count_str = line.split(":")[-1].strip().split()[0]
+            new_face_count = int(new_face_count_str)
+
+            # Update the face count with the maximum value
+            face_count = max(face_count, new_face_count)
 
     # Calculate seatbelt rating
-    seatbelt_rating = seatbelt_count / (seatbelt_count + seatbelt_notcount) * 100  # Assuming 30 frames per second
+    seatbelt_rating = (
+        seatbelt_count / (seatbelt_count + seatbelt_notcount) * 100
+    )  # Assuming 30 frames per second
     print(f"Seatbelt Rating: {seatbelt_rating:.2f}/100")
     print(f"Smoking Detected: {smoking_detected}")
     print(f"Drinking Detected: {drinking_detected}")
     print(f"Drowsiness Detected: {drowsiness_detected}")
-    
+    print(f"Number of faces detected: {face_count}")
+
     cap.release()
     cv2.destroyAllWindows()
     print("Script run complete.")
